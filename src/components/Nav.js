7dd8a4a2431121ab3782cbfb2872726bff788ad6
aldairbalanzar <abalanzar7@gmail.com>
2020-08-30 03:43:25 -0400

    added navbar animation

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -11,51 +11,66 @@ const Nav = () => {
     }
 
     const variants = {
-        burger: {},
-        times: {},
-        openBurger: {opacity: 1, x: 0 },
-        closedBurger: { opacity: 0, x: '-10%'},
-        open: {opacity: 1, x: 0 },
-        closed: { opacity: 0, x: '-100%' },
+        openMenu: {width: '15%'},
+        closedMenu: {width: '6%'},
+
+        barsDisplay: { opacity: 1, x: 0 },
+        barsHide: { opacity: 0, x: -100 },
+        timesDisplay: { opacity: 1, x: 0 },
+        timesHide: { opacity: 0, x: -100 }
     }
 
     return(
-        <div>
-            <motion.div onClick={handleNav}
+        <motion.div className='menu-container'
+            variants={variants}
+            style={{overflow: 'hidden'}}
+            initial='closedMenu'
+            animate={isNavOpen ? 'openMenu' : 'closedMenu'}
+        >
+            {/* bars icon */}
+            {!isNavOpen &&
+                <motion.i className={`menu-bars fa fa-bars`} onClick={handleNav}
                 variants={variants}
                 style={{overflow: 'hidden'}}
-                initial='closedBurger'
-                animate={isNavOpen ? 'closedBurger' : 'openBurger'}
-            >
-                <div onClick={handleNav}>
-                    <i className={'hamburger fa fa-bars'}></i>
-                </div>
-            </motion.div>
+                initial={{x: -30}}
+                animate={isNavOpen ? 'barsHide' : 'barsDisplay'}
+                transition={{ duration: .5 }}
+                ></motion.i>
+            }
+            
+            {/* times icon */}
+            {isNavOpen &&
+                <div>  
+                    <motion.i className={`menu-times fa fa-times`} onClick={handleNav}
+                    variants={variants}
+                    style={{overflow: 'hidden'}}
+                    initial={{x: -30}}
+                    animate={isNavOpen ? 'timesDisplay' : 'timesHide'}
+                    transition={{ duration: .5 }}
+                    ></motion.i>
 
-            <motion.nav
-                variants={variants}
-                style={{overflow: 'hidden'}}
-                initial='closed'
-                animate={isNavOpen ? 'open' : 'closed'}
-            >
-                <div onClick={handleNav}>
-                    <i className={'times fa fa-times'}></i>
+                    <motion.ul
+                    variants={variants}
+                    style={{overflow: 'hidden'}}
+                    initial={{x: -30}}
+                    animate={isNavOpen ? 'timesDisplay' : 'timesHide'}
+                    transition={{ duration: .5 }}
+                    >
+                        <li><a href="">Projects</a></li>
+                        <li><a href="">About Me</a></li>
+                        <li><a href="">Contact</a></li>
+                        <div className="nav-icon-container">
+                            <a href='https:www.linkedin.com/in/aldair-balanzar-390185177/' target='_blank' style={{margin: '0 3%'}}>
+                                <i className="nav-icon fa fa-linkedin-square"></i>
+                            </a>
+                            <a href='https:github.com/aldairbalanzar' target='_blank' style={{margin: '0 3%'}}>
+                                <i className="nav-icon fa fa-github"></i>
+                            </a>
+                        </div>
+                    </motion.ul>
                 </div>
-                <ul>
-                    <li><a href="">Projects</a></li>
-                    <li><a href="">About Me</a></li>
-                    <li><a href="">Contact</a></li>
-                    <div className="nav-icon-container">
-                        <a href='https://www.linkedin.com/in/aldair-balanzar-390185177/' target='_blank' style={{margin: '0 3%'}}>
-                            <i className="nav-icon fa fa-linkedin-square"></i>
-                        </a>
-                        <a href='https://github.com/aldairbalanzar' target='_blank' style={{margin: '0 3%'}}>
-                            <i className="nav-icon fa fa-github"></i>
-                        </a>
-                    </div>
-                </ul>
-            </motion.nav>
-        </div>
+            }
+        </motion.div>
     )
 }
 
